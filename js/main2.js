e5ef99c95e82f0322ab07a5a00228954da1f51b3
Carlos Munoz Diaz <carlos10md@gmail.com>
2021-07-25 17:23:46 +0200

    1

--- js/main2.js
+++ js/main2.js
@@ -2,9 +2,20 @@ let tooltip = d3.select('#chartTooltip');
 let charts = document.getElementsByClassName('chart__viz');
 
 function getOut() {
-    alert("Entra 2");
-    alert(charts[0]);
-    alert(tooltip);
+    //Bloque de la visualización
+    let chartBlock = d3.select('#chart-one');
+    
+    //Lectura de datos
+    let file = './data/chart-one.csv';
+    d3.csv(file, function(d) {
+        return {
+            Fecha: d.Year,
+            'América Latina y Caribe': +d['LAC'].replace(/,/g, '.')
+        }
+    }, function(error, data) {
+        if (error) throw alert(error);
+        alert(data);
+    });
 }
 
 getOut();
