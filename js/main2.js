90378be5d4b1b25b8601f3296f0383e01c6bc6f8
Carlos Munoz Diaz <carlos10md@gmail.com>
2021-07-25 16:22:50 +0200

    9

--- js/main2.js
+++ js/main2.js
@@ -2,8 +2,9 @@ let tooltip = d3.select('#chartTooltip');
 let charts = document.getElementsByClassName('chart__viz');
 
 function getOut() {
-    alert("Entra");
+    alert("Entra 2");
     alert(charts[0]);
+    alert(tooltip);
 }
 
 getOut();
